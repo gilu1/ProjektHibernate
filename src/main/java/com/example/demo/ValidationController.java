1a19e9f2b65dbcd26a7f81cb1b2ce165d927a7e3
gilu1 <gilus362@gmail.com>
2017-12-09 12:06:52 +0100

    poprawione klasy

--- src/main/java/com/example/demo/ValidationController.java
+++ src/main/java/com/example/demo/ValidationController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -7,6 +9,7 @@ import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,27 +19,31 @@ public class ValidationController {
 
 	@Autowired
 	Validator validator;
-	@ResponseBody
+	//@ResponseBody 
 	@RequestMapping(value = "/validate", method = RequestMethod.GET)
 
-	public String validateBook() {
+	
+	public String validateBook(Model model) {
 		Book book = new Book();
 		book.setTitle("Test");
 		book.setRating(20);
 		book.setAuthor(" ");
 
 		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		List<String> list1 = new ArrayList<>();
 		if (!violations.isEmpty()) {
 			for (ConstraintViolation<Book> constraintViolation : violations) {
 				System.out.println(constraintViolation.getPropertyPath() + "	" + constraintViolation.getMessage());
+				list1.add(constraintViolation.getPropertyPath() + "	" + constraintViolation.getMessage());
 			}
+			model.addAllAttributes(list1);
 		} else {
 			// save object }
 
-			return "aa";
+			return "validate";
 
 		}
-		return "aa";
+		return "validate";
 	}
 
 }
